737a6de3e775f1817f75197fa68b999c72cb673e
pibahamondes <pedro.bahamondes@polytechnique.edu>
2017-04-17 10:20:44 +0200

    Incluye ahora mostrar los posts, con hora, nombre de usuario y título

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const request = require('request');
-const yargs = require('yargs');
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const he = require('he');
+
+const request = require('request');
+
+const publicPath = path.join(__dirname, 'mainsite');
+const port = process.env.PORT || 8080;
 
 var app = express();
-
-
-
-hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(publicPath));
 
-hbs.registerHelper('getYear', ()=> new Date().getFullYear());
-hbs.registerHelper('screamIt', text => text.toUpperCase());
+hbs.registerHelper('heDecode', (text) => text ? he.decode(text) : undefined);
+app.get('/readMore', (req, res) => {
+  res.render('../mainsite/readMore.hbs');
+})
+hbs.registerHelper('readMore1', (art) => fifi(art));
+
 var title = art => art.title ? art.title : art.story_title ? art.story_title : 'Untitled';
-hbs.registerHelper('titleArt', art => title(art));
 var body = art => art.comment_text ? art.comment_text : art.story_text;
-hbs.registerHelper('bodyArt', art => body(art));
 var printArticle = (art) => {
   var s = `Author: ${art.author}\n`;
   s += `Title: ${title(art)}\n`;
@@ -27,27 +30,29 @@ var printArticle = (art) => {
   console.log('--------');
   return s;
 };
-hbs.registerHelper('hitsobj', (hits) => {
+
+var printHits = (hits) => {
   var text = "";
   hits.forEach((art) => {
     text += printArticle(art);
   });
   return text;
-});
-
-const API = 'http://hn.algolia.com/api/v1/search_by_date?query=nodejs';
+};
 
 app.get('/', (req, res) => {
-  request({
-    url: API,
-    json: true
-  }, (error, response, body) =>{
-    res.render('index.hbs',{
-      pageTitle: 'Home',
-      welcomeMessage: 'Welcome to this nice website!',
+const API = 'http://hn.algolia.com/api/v1/search_by_date?query=nodejs';
+request({url: API, json: true}, (error, response, body) =>{
+    //printHits(body.hits);
+    res.render('../mainsite/index.hbs', {
       hits: body.hits
     });
-  });
+  }
+);
 });
 
-app.listen(8080);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
---
+++ mainsite/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on('connect', function(){
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function(){
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+  console.log('newMessage', message);
+});
+
+socket.on('newEmail', function(email){
+  console.log('New email', email);
+});
